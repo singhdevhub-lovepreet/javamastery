d5e9be473dabc3c08a3e55689a37d9725995fcf0
singhdevhub-lovepreet <singhdevhub@gmail.com>
2025-01-02 00:28:50 +0530

    removing redundant code

---
+++ src/main/java/com/singhdevhub/assignments/Car.java
@@ -0,0 +1,14 @@
+package com.singhdevhub.assignments;
+
+public class Car {
+    
+    /*
+     * @Topic:-
+     * What are classes and Objects 
+     * defining methods in classes
+     */
+
+    
+    
+
+}
