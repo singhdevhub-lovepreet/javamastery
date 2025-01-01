d533ccdfbd12b81e60b2850a529d02dd4c34efc9
singhdevhub-lovepreet <singhdevhub@gmail.com>
2025-01-02 00:48:31 +0530

    Adding class methods

--- src/main/java/com/singhdevhub/App.java
+++ src/main/java/com/singhdevhub/App.java
@@ -1,8 +1,14 @@
 package com.singhdevhub;
 
+import com.singhdevhub.assignments.Car;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Project setuped successfully");
+        
+        Car car = new Car();
+        car.setCarName("BMW");
+        System.out.println(car.getCarName());
+
     }
 }
--- src/main/java/com/singhdevhub/assignments/Car.java
+++ src/main/java/com/singhdevhub/assignments/Car.java
@@ -3,12 +3,33 @@ package com.singhdevhub.assignments;
 public class Car {
     
     /*
-     * @Topic:-
-     * What are classes and Objects 
-     * defining methods in classes
+     * @Assignment:- 2
+     * @Description:- classes and Objects, defining methods in classes
+     */
+    
+    // -------// -------- // -------- // -------- 
+
+    /*
+     * @Description:-
+     * Input:- String
+     * Output:- None
      */
 
+    private String carName;
     
+    public void setCarName(String carName){
+        this.carName = carName;
+    }
+
+    /*
+     * @Description:-
+     * Input:- None
+     * Output:- return car name
+     */
+
+    public String getCarName(){
+        return this.carName;
+    }
     
 
 }
