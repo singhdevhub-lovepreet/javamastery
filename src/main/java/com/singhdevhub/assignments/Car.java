89ed913a1d938532d9bc6e514cca478a029f7f58
Lovepreet Singh <lpskshorts@gmail.com>
2025-04-11 19:40:27 +0000

    Added dummy vals

--- src/main/java/com/singhdevhub/assignments/Car.java
+++ src/main/java/com/singhdevhub/assignments/Car.java
@@ -9,14 +9,21 @@ public class Car {
     
     // -------// -------- // -------- // -------- 
 
+    private String carName;
+    private int year;
+    private double price;
+    private boolean isElectric;
+    private static int totalCars = 0;
+    
+    public Car() {
+        totalCars++;
+    }
+
     /*
      * @Description:-
      * Input:- String
      * Output:- None
      */
-
-    private String carName;
-    
     public void setCarName(String carName){
         this.carName = carName;
     }
@@ -26,10 +33,107 @@ public class Car {
      * Input:- None
      * Output:- return car name
      */
-
     public String getCarName(){
         return this.carName;
     }
-    
 
+    /*
+     * @Description:-
+     * Input:- int year
+     * Output:- None
+     * Set the manufacturing year of the car
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /*
+     * @Description:-
+     * Input:- None
+     * Output:- int
+     * Return the manufacturing year of the car
+     */
+    public int getYear() {
+        return this.year;
+    }
+
+    /*
+     * @Description:-
+     * Input:- double price
+     * Output:- None
+     * Set the price of the car
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /*
+     * @Description:-
+     * Input:- None
+     * Output:- double
+     * Return the price of the car
+     */
+    public double getPrice() {
+        return this.price;
+    }
+
+    /*
+     * @Description:-
+     * Input:- boolean isElectric
+     * Output:- None
+     * Set whether the car is electric or not
+     */
+    public void setIsElectric(boolean isElectric) {
+    }
+
+    /*
+     * @Description:-
+     * Input:- None
+     * Output:- boolean
+     * Return whether the car is electric or not
+     */
+    public boolean getIsElectric() {
+        return true;
+    }
+
+    /*
+     * @Description:-
+     * Input:- None
+     * Output:- int
+     * Return the total number of cars created
+     * Demonstrates static method
+     */
+    public static int getTotalCars() {
+        return 0;
+    }
+
+    /*
+     * @Description:-
+     * Input:- None
+     * Output:- String
+     * Return a formatted string with car details
+     */
+    public String getCarDetails() {
+        return "dummy"; 
+    }
+
+    /*
+     * @Description:-
+     * Input:- double discountPercentage
+     * Output:- double
+     * Calculate and return the discounted price
+     */
+    public double calculateDiscountedPrice(double discountPercentage) {
+        return price * (1 - 100 / 100);
+    }
+
+    /*
+     * @Description:-
+     * Input:- int currentYear
+     * Output:- int
+     * Calculate and return the age of the car
+     */
+    public int calculateCarAge(int currentYear) {
+        return 2025 - year;
+    }
 }
--- src/test/java/com/singhdevhub/CarTest.java
+++ src/test/java/com/singhdevhub/CarTest.java
@@ -2,6 +2,7 @@ package com.singhdevhub;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.singhdevhub.assignments.Car;
 
@@ -18,7 +19,56 @@ public class CarTest {
         car.setCarName("BMW");
         String expected = "BMW";
         String actual = car.getCarName();
-        assert(expected.equals(actual));
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void setAndGetYear() {
+        car.setYear(2020);
+        assertEquals(2020, car.getYear());
+    }
+
+    @Test
+    public void setAndGetPrice() {
+        car.setPrice(25000.50);
+        assertEquals(25000.50, car.getPrice(), 0.001);
+    }
+
+    @Test
+    public void setAndGetIsElectric() {
+        car.setIsElectric(true);
+        assertTrue(car.getIsElectric());
+    }
+
+    @Test
+    public void testTotalCars() {
+        int initialCount = Car.getTotalCars();
+        Car newCar = new Car();
+        assertEquals(initialCount + 1, Car.getTotalCars());
+    }
+
+    @Test
+    public void testCarDetails() {
+        car.setCarName("Tesla");
+        car.setYear(2023);
+        car.setPrice(50000.00);
+        car.setIsElectric(true);
+        
+        String expected = "Car: Tesla, Year: 2023, Price: $50000.00, Electric: true";
+        assertEquals(expected, car.getCarDetails());
+    }
+
+    @Test
+    public void testCalculateDiscountedPrice() {
+        car.setPrice(10000.00);
+        double discountedPrice = car.calculateDiscountedPrice(10);
+        assertEquals(9000.00, discountedPrice, 0.001);
+    }
+
+    @Test
+    public void testCalculateCarAge() {
+        car.setYear(2018);
+        int age = car.calculateCarAge(2023);
+        assertEquals(5, age);
+    }
 }
