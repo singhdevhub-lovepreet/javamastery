61e54fefa32ba1e530bd6749cb5b9d022fe10541
lovepreet-singh_dtpl <lovepreet.singh@cred.club>
2025-05-27 22:52:52 +0530

    removed solved code

--- src/main/java/com/singhdevhub/assignments/LoopsAndRecursion.java
+++ src/main/java/com/singhdevhub/assignments/LoopsAndRecursion.java
@@ -29,21 +29,9 @@ public class LoopsAndRecursion {
      */
 
      public void printStarPattern(){
-        for(int i = 1; i <= 9; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
     
     private BigDecimal calculateSumOfFirstNNumbers(int n){
-        BigDecimal sum = new BigDecimal(0);
-        for(int i = 1; i <= n; i++){
-            sum = sum.add(new BigDecimal(i));
-        }
-        System.out.println("Sum of first " + n + " natural numbers is: " + sum); 
-        return sum;   
     }
         
     
@@ -64,10 +52,6 @@ public class LoopsAndRecursion {
      * Extras:- this is about calling a function inside a function
      */
     public BigDecimal calculateFactorial(int n){
-        if(n==0 || n==1){
-            return new BigDecimal(1);
-        }
-        return new BigDecimal(n).multiply(calculateFactorial(n-1));
     }
 
 }
